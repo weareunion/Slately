0b6d6351e7935722c0fcf01f66801a4c7e467b24
Union Development, LLC <uniondevelopment.gac@gmail.com>
2020-12-26 20:46:20 -0500

    Changed default Port for client

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   callAPI() {
-    fetch("http://localhost:9000/testAPI")
+    fetch("http://localhost:3000/testAPI")
         .then(res => res.text())
         .then(res => this.setState({ apiResponse: res }));
   }
