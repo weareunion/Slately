c51054ec4e1cf1a5765d334302eb062814d2b73e
Union Development, LLC <uniondevelopment.gac@gmail.com>
2020-12-29 19:18:06 -0500

    Added Rollbar Support

--- server/app/services/Rollbar/rollbar.js
+++ server/app/services/Rollbar/rollbar.js
@@ -3,7 +3,7 @@ const rollbar = new Rollbar({
     accessToken: '4816aec76536488cad0f8ae679e58b64',
     captureUncaught: true,
     captureUnhandledRejections: true,
-    verbose: (process.env.ENVIRONMENT === 'development')
+    verbose: (process.env.STAGE === 'development')
 });
 
 module.exports = rollbar;
